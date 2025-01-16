28c6150f0c78b6436081dfee194df849183dd131
Minal Meshram <minalmeshram55@gmail.com>
2025-01-16 22:11:08 +0530

    updated1

--- email-writer-backend/src/main/java/com/email/writer/app/EmailGeneratorController.java
+++ email-writer-backend/src/main/java/com/email/writer/app/EmailGeneratorController.java
@@ -1,17 +1,16 @@
 package com.email.writer.app;
 
-import com.email.writer.app.EmailRequest;
+
 import lombok.AllArgsConstructor;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/api/email")
 @AllArgsConstructor
+@CrossOrigin(origins = "*")
 public class EmailGeneratorController {
+
     private final EmailGeneratorService emailGeneratorService;
     @PostMapping("/generate")
     public ResponseEntity<String> generateEmail(@RequestBody EmailRequest emailRequest){
